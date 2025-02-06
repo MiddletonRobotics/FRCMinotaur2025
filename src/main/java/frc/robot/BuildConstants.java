641e9eb86d121a87ee356779ffccb4458c1c556a
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2025-02-06 12:57:23 -0500

    updaing vendordeps

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRCMinotaur2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 4;
-  public static final String GIT_SHA = "ee6b07b1337c07cb9135d28d84a03a2b0f553e78";
-  public static final String GIT_DATE = "2025-01-24 09:45:49 EST";
+  public static final int GIT_REVISION = 7;
+  public static final String GIT_SHA = "88995a903e5d6ae84ed59cf61a160c51679ccc5e";
+  public static final String GIT_DATE = "2025-02-06 12:55:14 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-02-06 12:10:47 EST";
-  public static final long BUILD_UNIX_TIME = 1738861847432L;
+  public static final String BUILD_DATE = "2025-02-06 12:56:56 EST";
+  public static final long BUILD_UNIX_TIME = 1738864616320L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
