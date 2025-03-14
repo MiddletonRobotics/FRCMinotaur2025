af889ba480e1e97bc75d4a62c7e74a9151cf7b22
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2025-03-14 14:29:10 -0400

    more changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRCMinotaur2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 53;
-  public static final String GIT_SHA = "c9d107c067e0d09626417fb44c2525c45a43065a";
-  public static final String GIT_DATE = "2025-03-14 04:27:29 EDT";
+  public static final int GIT_REVISION = 54;
+  public static final String GIT_SHA = "f9407a81ed79f47b06c0b8fb82824061f70f9ef4";
+  public static final String GIT_DATE = "2025-03-14 14:27:09 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-14 13:58:28 EDT";
-  public static final long BUILD_UNIX_TIME = 1741975108715L;
+  public static final String BUILD_DATE = "2025-03-14 14:27:48 EDT";
+  public static final long BUILD_UNIX_TIME = 1741976868501L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
