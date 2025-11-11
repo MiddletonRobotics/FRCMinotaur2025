631abcb031b0b9bf5eb1c180620a4cfbe0021ff9
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2025-11-10 22:16:50 -0500

    remove this not needed

--- src/main/java/frc/robot/commands/LEDCommands.java
+++ src/main/java/frc/robot/commands/LEDCommands.java
@@ -1,6 +1,12 @@
 package frc.robot.commands;
 
+import com.ctre.phoenix6.controls.LarsonAnimation;
+import com.ctre.phoenix6.signals.RGBWColor;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -9,11 +15,13 @@ import frc.robot.utilities.BlinkinLEDController.BlinkinPattern;
 import frc.robot.utilities.constants.Constants;
 
 public class LEDCommands {
-    public static Command intakenAlgea(LEDSubsystem ledSubsystem) {
-        return new SequentialCommandGroup(
-            new InstantCommand(() -> ledSubsystem.setPattern(BlinkinPattern.STROBE_BLUE)),
-            new WaitCommand(1.25),
-            new InstantCommand(() -> ledSubsystem.setPattern(Constants.DriverConstants.DEF_PATTERN))
-        );
+    /* 
+    public static Command robotReadyForMatch(LEDSubsystem ledSubsystem) {
+        return Commands.runOnce(() -> {
+            ledSubsystem.setPattern(new LarsonAnimation(8, 76).withColor(
+                DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red ? RGBWColor.fromHex("#FF0000").get() : RGBWColor.fromHex("#FF0000").get()
+            ));
+        }, ledSubsystem);
     }
+        */
 }
