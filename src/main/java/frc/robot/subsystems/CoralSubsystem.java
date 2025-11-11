63b6eef2b8dbd3cec34edb675a2bf57f3b3e15ba
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2025-11-10 22:16:03 -0500

    added CANRange, and booleans for coral state

--- src/main/java/frc/robot/subsystems/CoralSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralSubsystem.java
@@ -1,5 +1,14 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.CANrangeConfiguration;
+import com.ctre.phoenix6.configs.ProximityParamsConfigs;
+import com.ctre.phoenix6.controls.ControlRequest;
+import com.ctre.phoenix6.hardware.CANrange;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
@@ -8,6 +17,7 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Alert.AlertType;
@@ -30,9 +40,17 @@ public class CoralSubsystem extends SubsystemBase {
     private Alert rollerOverCurrent;
     private Alert rollerSensorDisconnected;
 
-    private DigitalInput firstBeamBreak;
+    private CANrange canrange;
     private DigitalInput secondBeamBreak;
 
+    private CANrangeConfiguration configuration;
+
+    private final StatusSignal<Double> beamBreakSignalStrength;
+    private final StatusSignal<Boolean> beamBreakTripped;
+    private final StatusSignal<Distance> beamBreakDistance;
+
+    private boolean currentlyHasCoral = false;
+    private boolean currentlyIsStaging = false;
 
     public CoralSubsystem() {
         coralSpinnerMotor = new SparkMax(21, MotorType.kBrushless);
@@ -41,8 +59,20 @@ public class CoralSubsystem extends SubsystemBase {
         CoralSpinnerConfiguration = new SparkMaxConfig();
         configureCoralSpinner();
 
-        firstBeamBreak = new DigitalInput(1);
+        configuration = new CANrangeConfiguration()
+        .withProximityParams(new ProximityParamsConfigs()
+            .withProximityHysteresis(0.001)
+            .withProximityThreshold(0.11).withMinSignalStrengthForValidMeasurement(5500)
+        );
+
+        canrange = new CANrange(24);
         secondBeamBreak = new DigitalInput(2);
+
+        canrange.getConfigurator().apply(configuration);
+
+        beamBreakSignalStrength = canrange.getSignalStrength();
+        beamBreakTripped = canrange.getIsDetected();
+        beamBreakDistance = canrange.getDistance();
 
         rollerCANDisconnected = new Alert("Coral CAN Disconnect. Will not function.", AlertType.kError);
         rollerOverTempurature = new Alert("Coral Over Tempurature", AlertType.kWarning);
@@ -78,22 +108,42 @@ public class CoralSubsystem extends SubsystemBase {
     }
 
     public boolean firstBeamBroken() {
-        return !firstBeamBreak.get();
+        return beamBreakTripped.getValue();
     }
 
     public boolean secondBeamBreak() {
         return !secondBeamBreak.get();
     }
 
+    public boolean currentlyHasCoral() {
+        return currentlyHasCoral;
+    }
+
+    public boolean currentlyIsStaging(){
+        return currentlyIsStaging;
+    }
+
+    public void setCurrentlyHasCoral(boolean holding) {
+        currentlyHasCoral = holding;
+    }
+
+    public void setCurrentlyIsStaging(boolean staging) {
+        currentlyIsStaging = staging;
+    }
+    
+
     @Override
     public void periodic() {
         updateLogs();
         updateAlerts();
+
+        BaseStatusSignal.refreshAll(beamBreakDistance, beamBreakSignalStrength, beamBreakTripped);
     }
 
     public void updateLogs() {
-        SmartDashboard.putBoolean("Coral First Beam Break", firstBeamBroken());
-        SmartDashboard.putBoolean("Coral Second Beam Break", secondBeamBreak());
+        SmartDashboard.putBoolean("Coral First Beam Broken", firstBeamBroken());
+        SmartDashboard.putNumber("Coral First Beam Distance", beamBreakDistance.getValue().in(Meters));
+        SmartDashboard.putBoolean("Coral Second Beam Broken", secondBeamBreak());
     }
 
     public void updateAlerts() {
