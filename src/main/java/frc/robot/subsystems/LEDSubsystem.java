2c60e39521c086b48e2e51426cfa966e5739be4c
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2025-11-10 22:15:44 -0500

    changed led subsystem to use CANdle

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -1,23 +1,32 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.CANdleConfiguration;
+import com.ctre.phoenix6.configs.CANdleFeaturesConfigs;
+import com.ctre.phoenix6.configs.LEDConfigs;
+import com.ctre.phoenix6.controls.ControlRequest;
+import com.ctre.phoenix6.controls.EmptyAnimation;
+import com.ctre.phoenix6.hardware.CANdle;
+import com.ctre.phoenix6.signals.StripTypeValue;
+
 import frc.robot.utilities.BlinkinLEDController;
 import frc.robot.utilities.BlinkinLEDController.BlinkinPattern;
 
 public class LEDSubsystem {
-    private BlinkinLEDController ledController;
+    private CANdle candle;
+    private CANdleConfiguration configuration;
 
     public LEDSubsystem() {
         System.out.println("[Initialization] Creating LEDSubsystem");
 
-        ledController = BlinkinLEDController.getInstance();
-        setAllianceColor();
+        candle = new CANdle(23, "*");
+        configuration = new CANdleConfiguration()
+            .withLED(new LEDConfigs().withStripType(StripTypeValue.GRBW)
+        );
+
+        candle.getConfigurator().apply(configuration);
     }
 
-    public void setAllianceColor() {
-        ledController.setAllianceColorBreath();
-    }
-
-    public void setPattern(BlinkinPattern pattern) {
-        ledController.setPattern(pattern);
+    public void setPattern(ControlRequest request) {
+        candle.setControl(request);
     }
 }
